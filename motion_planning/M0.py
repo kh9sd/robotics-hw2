e31f8e7768076b3cab211b46e08d933744312df0
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2024-02-25 19:01:07 -0500

    m0 trials

--- motion_planning/M0.py
+++ motion_planning/M0.py
@@ -9,4 +9,14 @@ def M0() -> np.array:
     np.array
         1x4 numpy array of joint angles
     """
+    # return  np.array([np.pi/4, 0, 0, 0])
+    # return  np.array([0, np.pi/4, 0, 0])
+    # return  np.array([0, 0, np.pi/4, 0])
+    # return  np.array([0, 0, 0, np.pi/4])
+
+    # return  np.array([np.pi/2, np.pi/4, 0, 0])
+    # return  np.array([0, np.pi/4, np.pi/2, 0])
+
+    # return  np.array([np.pi/4, np.pi/4, np.pi/4, 0])
+
     return  np.array([0, -np.pi/4, 0, -np.pi/4])
