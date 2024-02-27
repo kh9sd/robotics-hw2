95eb714665c8b94ce47b58794ce34719ee4a8489
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2024-02-26 21:30:25 -0500

    attempt at c7

--- cspace/C7.py
+++ cspace/C7.py
@@ -4,6 +4,47 @@ import math
 from matplotlib.patches import Polygon
 from helper_functions import *
 
+import C3
+
+def pad_by_1_helper(cspace: np.array):
+    """Pad the configuration space by one grid cell.
+
+    Parameters
+    ----------
+    cspace : np.array
+        The origianl configuration space of the robot.
+
+    Returns
+    -------
+    np.array
+        The padded configuration space of the robot.
+    """
+    padded_cspace = cspace.copy()
+    # element in cspace is 1 if in collision, 0 if not
+
+    assert padded_cspace.ndim == 2
+    for cur_x in range(cspace.shape[0]):
+        for cur_y in range(cspace.shape[1]):
+            neighbor_candidates =  C3.filter_out_neighbors(cspace.shape, 
+                                                           [[cur_x - 1, cur_y-1],
+                                                            [cur_x - 1, cur_y],
+                                                            [cur_x - 1, cur_y+1],
+
+                                                            [cur_x, cur_y-1],
+                                                            # [cur_x, cur_y],
+                                                            [cur_x, cur_y+1],
+
+                                                            [cur_x + 1, cur_y-1],
+                                                            [cur_x + 1, cur_y],
+                                                            [cur_x + 1, cur_y+1]])
+            
+            if math.isclose(cspace[cur_x, cur_y], 1):
+                for neighbor_x, neighbor_y in neighbor_candidates:
+                    padded_cspace[neighbor_x, neighbor_y] = 1
+
+    return padded_cspace
+
+
 def C7_func(cspace: np.array) -> np.array:
     """Pad the configuration space by one grid cell.
 
@@ -19,6 +60,6 @@ def C7_func(cspace: np.array) -> np.array:
     """
 
     ### Insert your code below: ###
-    padded_cspace = cspace.copy()
     
+    padded_cspace = pad_by_1_helper(cspace)
     return padded_cspace
