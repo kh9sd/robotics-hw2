e843d2d87f247839e3ac6cd73684208fc42ba0dc
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2024-02-29 15:23:40 -0500

    m4 impl

--- motion_planning/M4.py
+++ motion_planning/M4.py
@@ -3,6 +3,35 @@ import numpy as np
 from robot import Simple_Manipulator as Robot
 import typing
 
+import math
+import random
+from networkx import Graph
+import networkx
+import M2
+
+def getClosestNode(graph: Graph, cur_sample: typing.Tuple[float, float, float, float]) -> typing.Tuple[float, float, float, float]:
+    return min((node for node in graph), key=lambda n: math.dist(cur_sample, n))
+
+
+def nextConfig(closest_vertex: typing.Tuple[float, float, float, float], cur_sample: typing.Tuple[float, float, float, float], max_step: float) -> typing.Tuple[float, float, float, float]:
+    if math.dist(cur_sample, closest_vertex) > max_step:
+        from_v = np.array(closest_vertex)
+        to_v = np.array(cur_sample)
+        change_v = to_v - from_v
+        norm_change_v = change_v / np.linalg.norm(change_v)
+
+        return tuple(from_v + (norm_change_v * max_step))
+    else:
+        return cur_sample
+
+
+def rrt_random_sample(robot, q_goal: np.array) -> typing.Tuple[float, float, float, float]:
+    if random.randrange(0, 10) == 0:
+        return tuple(q_goal)
+    else:
+        return tuple(M2.generate_random_config(robot.lower_lims, robot.upper_lims))
+
+
 def M4(robot: Robot, q_start: np.array, q_goal: np.array) -> typing.Tuple[np.array, bool]:
     """Implement RRT algorithm to find a path from q_start to q_goal
 
@@ -26,7 +55,22 @@ def M4(robot: Robot, q_start: np.array, q_goal: np.array) -> typing.Tuple[np.arr
             Boolean denoting whether a path was found
     """
 
-    # student work start here
-    raise NotImplementedError
-                        
-    return path, path_found
+    MAX_ITERATIONS = 500
+    MAX_STEP_SIZE = 1
+    graph = Graph()
+    graph.add_node(tuple(q_start))
+
+    for _ in range(MAX_ITERATIONS):
+        cur_sample = rrt_random_sample(robot, q_goal)
+        closest_vertex = getClosestNode(graph, cur_sample)
+        next_config = nextConfig(closest_vertex, cur_sample, MAX_STEP_SIZE)
+
+        if robot.check_edge(np.array(closest_vertex), np.array(next_config)):
+            graph.add_edge(closest_vertex, next_config)
+
+    path = networkx.shortest_path(graph, tuple(q_start), tuple(q_goal))
+
+    if not path:
+        return np.zeros((1,4)), False
+    else:
+        return np.array(path), True
